3d8878fc767a3688dcb75f3ee012ad0fa4f41901
Jon Craig <jon.craig@stargatestudios.net>
2021-11-26 22:29:11 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const Gpio = require('pigpio').Gpio;
-
-const motor = new Gpio(11, {mode: Gpio.OUTPUT});
-
+const motor = new Gpio(11, {
+  mode: Gpio.OUTPUT
+});
 let pulseWidth = 1000;
 let increment = 100;
-
 setInterval(() => {
   motor.servoWrite(pulseWidth);
-
+  console.log(pulseWidth);
   pulseWidth += increment;
   if (pulseWidth >= 2000) {
     increment = -100;
