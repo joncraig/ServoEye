b209da1ce0c8dde3f38e6e9bba6d33d5a5bafeac
Jon Craig <jon.craig@stargatestudios.net>
2021-11-26 22:28:23 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,17 @@
 const Gpio = require('pigpio').Gpio;
 
-// The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
-const MICROSECDONDS_PER_CM = 1e6/34321;
+const motor = new Gpio(11, {mode: Gpio.OUTPUT});
 
-const trigger = new Gpio(23, {mode: Gpio.OUTPUT});
-const echo = new Gpio(24, {mode: Gpio.INPUT, alert: true});
+let pulseWidth = 1000;
+let increment = 100;
 
-trigger.digitalWrite(0); // Make sure trigger is low
-
-const watchHCSR04 = () => {
-  let startTick;
-
-  echo.on('alert', (level, tick) => {
-    if (level == 1) {
-      startTick = tick;
-    } else {
-      const endTick = tick;
-      const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
-      console.log(diff / 2 / MICROSECDONDS_PER_CM);
-    }
-  });
-};
-
-watchHCSR04();
-
-// Trigger a distance measurement once per second
 setInterval(() => {
-  trigger.trigger(10, 1); // Set trigger high for 10 microseconds
-}, 1000);
+  motor.servoWrite(pulseWidth);
+
+  pulseWidth += increment;
+  if (pulseWidth >= 2000) {
+    increment = -100;
+  } else if (pulseWidth <= 1000) {
+    increment = 100;
+  }
+}, 1000)
