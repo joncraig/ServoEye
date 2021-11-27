4cfe1c2e79dfc02eb8e1289d568871fa24a72577
Jon Craig <jon.craig@stargatestudios.net>
2021-11-26 21:18:04 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const raspi = require('raspi');
 const pwm = require('raspi-soft-pwm');
 let servo1;
 raspi.init(() => {
-  servo1 = new pwm.SoftPWM('GPIO11');
+  servo1 = new pwm.SoftPWM('GPI22');
   cycleServos();
 });
 
 function cycleServos() {
+console.log(0);
   servo1.write(0);
   setTimeout(() => {
+    console.log(1);
     servo1.write(1);
     setTimeout(cycleServos, 2000);
   }, 2000)
