6ab1293e163697508b190592df37f730417c6efc
Jon Craig <jon.craig@stargatestudios.net>
2021-11-26 22:01:48 -0800

    Update app.js

--- app.js
+++ app.js
@@ -34,10 +34,10 @@ function angleToPercent(angle) {
 var curAngle = 0;
 var direction = 1;
 setInterval(() => {
-  piblaster.setServoPwm("GPIO17", angleToPercent(curAngle)/100.0);
-  piblaster.setPwm("GPIO22", angleToPercent(curAngle)/100.0);
-  piblaster.setPwm("GPIO23", angleToPercent(curAngle)/100.0);
-  piblaster.setPwm("GPIO24", angleToPercent(curAngle)/100.0);
+  piblaster.setServoPwm("GPIO-17", angleToPercent(curAngle)/100.0);
+  piblaster.setPwm("GPIO-22", angleToPercent(curAngle)/100.0);
+  piblaster.setPwm("GPIO-23", angleToPercent(curAngle)/100.0);
+  piblaster.setPwm("GPIO-24", angleToPercent(curAngle)/100.0);
   console.log("Setting angle at: ", curAngle, angleToPercent(curAngle) + "%");
   curAngle += direction;
   // Change direction when it exceeds the max angle.
