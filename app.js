68b495405e27e8a274a27dca5a9b3d0744293e5a
Jon Craig <jon.craig@stargatestudios.net>
2021-11-28 12:59:41 -0800

    Update app.js

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ const motor24 = new Gpio(24, {
 const motor25 = new Gpio(25, {
   mode: Gpio.OUTPUT
 });
-let pulseWidth = 1000;
-let min = 500;
-let max = 2500;
+let deg = 0.0;
+let min = -90.0;
+let max = 90.0;
 let increment = 1.0;
 let dir = 1;
 const step = 0.5;
@@ -26,17 +26,17 @@ doStep();
 function doStep() {
   const delta = Date.now() - last;
   last = Date.now();
-  if (pulseWidth >= max) {
+  if (deg >= max) {
     dir = -1;
-    console.log(dir, pulseWidth)
-  } else if (pulseWidth <= min) {
+    console.log(dir, deg)
+  } else if (deg <= min) {
     dir = 1;
-    console.log(dir, pulseWidth)
+    console.log(dir, deg)
   }
   const variStep = delta * step * dir;
-  pulseWidth = Math.round(pulseWidth + variStep);
-  pulseWidth = Math.min(Math.max(pulseWidth, min), max);
-console.log(pulseWidth);
+  deg = Math.round(deg + variStep);
+  const pulseWidth = degToPw(deg);
+  console.log(pulseWidth);
   motor17.servoWrite(pulseWidth);
   motor22.servoWrite(pulseWidth);
   motor24.servoWrite(pulseWidth);
@@ -47,6 +47,9 @@ console.log(pulseWidth);
 function degToPw(deg) {
   const minP = 500;
   const minD = -90;
-  const maxP = 2000;
-  const maxD = 500;
+  const maxP = 2500;
+  const maxD = 90;
+  let pulseWidth = Math.round(deg / (maxD - minD)) * (maxP - minP))
+pulseWidth = Math.min(Math.max(pulseWidth, minP), maxP);
+return pulseWidth;
 }
