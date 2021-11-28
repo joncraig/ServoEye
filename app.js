21c80157ea62a81f52b5defa306c7c1f78accbbf
Jon Craig <jon.craig@stargatestudios.net>
2021-11-28 12:37:28 -0800

    Update app.js

--- app.js
+++ app.js
@@ -40,5 +40,5 @@ function doStep() {
   motor23.servoWrite(pulseWidth);
   motor24.servoWrite(pulseWidth);
   motor25.servoWrite(pulseWidth);
-  setTimeout(doStep, delta / 1);
+  setTimeout(doStep, delta / 4);
 }
