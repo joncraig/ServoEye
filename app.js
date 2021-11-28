6fd992bec28a577033031f3b8a3762356889bbf9
Jon Craig <jon.craig@stargatestudios.net>
2021-11-28 14:21:41 -0800

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,33 @@ const osc = require("osc-min");
 const sock = dgram.createSocket("udp4", function (msg, rinfo) {
   var error, error1;
   try {
-    return console.log(osc.fromBuffer(msg));
+    onMessage(osc.fromBuffer(msg));
   } catch (error1) {
     error = error1;
     return console.log("invalid OSC packet");
   }
 });
+let deg = 0.0;
+let min = -90.0;
+let max = 90.0;
+let increment = 1.0;
+let dir = 1;
+let step = 0.1;
+let rate = 100;
 sock.bind(3333);
+
+function onMessage(msg) {
+  if (msg.oscType != 'message') {
+    console.log(msg);
+    return;
+  }
+  if (msg.address == '/step') {
+    step = msg.args[0].value;
+  }
+  if (msg.address == '/rate') {
+    rate = msg.args[0].value;
+  }
+}
 const motor17 = new Gpio(17, {
   mode: Gpio.OUTPUT
 });
@@ -23,12 +43,6 @@ const motor24 = new Gpio(24, {
 const motor25 = new Gpio(25, {
   mode: Gpio.OUTPUT
 });
-let deg = 0.0;
-let min = -90.0;
-let max = 90.0;
-let increment = 1.0;
-let dir = 1;
-const step = 0.1;
 let last = Date.now() - 1000;
 doStep();
 
@@ -48,7 +62,7 @@ function doStep() {
   motor22.servoWrite(pulseWidth);
   motor24.servoWrite(pulseWidth);
   motor25.servoWrite(pulseWidth);
-  setTimeout(doStep, 100);
+  setTimeout(doStep, rate);
 }
 
 function degToPw(deg) {
